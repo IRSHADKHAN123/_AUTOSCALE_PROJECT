3038fa728c3c0442da2eef5ba6bf478fb9349e3a
IRSHADKHAN123 <irshadkhanorai17@gmail.com>
2025-02-05 14:59:34 +0530

    9

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region = "ap-south-1" # Change the region as per your preference
 }
 
-resource "aws_instance" "myec2" {
-    ami = "ami-0d13e3e640877b0b9"
-    instance_type = "t2.micro"
-    
-    tags = {
-        Name = "ec2-instance"
-    }
-} 
+resource "aws_instance" "web_server_1" {
+  ami           = "ami-03c68e52484d7488f" # Replace with the AMI ID you want to use
+  instance_type = "t2.micro" # Change instance type if necessary
+
+  tags = {
+    Name = "web-Instance-1"
+  }
+}
+
+resource "aws_instance" "web_server_2" {
+  ami           = "ami-03c68e52484d7488f" # Replace with the AMI ID you want to use
+  instance_type = "t2.micro" # Change instance type if necessary
+
+  tags = {
+    Name = "web-Instance-2"
+  }
+}
