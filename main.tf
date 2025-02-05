d76b1a609ba1952fdc3c80a9dd1de25606c9c035
IRSHADKHAN123 <irshadkhanorai17@gmail.com>
2025-02-05 12:23:02 +0530

    6

--- main.tf
+++ main.tf
@@ -1,41 +1,12 @@
 provider "aws" {
-  region = "ap-south-1"  # Change this if needed
+  region     = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-03c68e52484d7488f"  # Amazon Linux 2 AMI (update based on region)
-  instance_type = "t2.micro"               # Free tier eligible
-
-  tags = {
-    Name = "ExampleEC2Instance"
-  }
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "ec2_with_sg" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
-  tags = {
-    Name = "EC2WithSecurityGroup"
-  }
-}
+resource "aws_instance" "myec2" {
+    ami = "ami-0d13e3e640877b0b9"
+    instance_type = "t2.micro"
+    
+    tags = {
+        Name = "ec2-instance"
+    }
+} 
